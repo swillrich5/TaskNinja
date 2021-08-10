e207f24efedcc35d65fd1d7a90d3520bf66cfdb4
swillrich5 <swillrich@gmail.com>
2021-08-10 15:44:18 -0500

    added Navbar and shell of Home.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home}/>
+      </Switch>
+    </Router>
   );
 }
 
